0902c2562fa2e90353be943480e49faea3374222
myyrakle <sssang97@naver.com>
2024-02-26 16:58:23 +0900

    config에 필드 추가

--- src/config/mod.rs
+++ src/config/mod.rs
@@ -13,6 +13,24 @@ pub struct RootConfig {
     domain_dir: PathBuf,
     internal_dir: PathBuf,
     entrypoint_file: PathBuf,
+
+    handler_struct_suffix: String,
+
+    usecase_struct_suffix: String,
+    usecase_interface_suffix: String,
+    request_dto_struct_suffix: String,
+    response_dto_struct_suffix: String,
+
+    store_variant_list: Vec<String>,
+    store_struct_suffix: String,
+    store_interface_suffix: String,
+    params_struct_suffix: String,
+
+    helper_struct_suffix: String,
+    helper_interface_suffix: String,
+    args_struct_suffix: String,
+    result_struct_suffix: String,
+
     framework: Framework,
 }
 
@@ -22,6 +40,24 @@ impl Default for RootConfig {
             domain_dir: PathBuf::new().join("./domain"),
             internal_dir: PathBuf::new().join("./internal"),
             entrypoint_file: PathBuf::new().join("./cmd/main.go"),
+            handler_struct_suffix: "Handler".to_string(),
+            usecase_struct_suffix: "Usecase".to_string(),
+            usecase_interface_suffix: "Usecase".to_string(),
+            request_dto_struct_suffix: "Request".to_string(),
+            response_dto_struct_suffix: "Response".to_string(),
+            store_struct_suffix: "Store".to_string(),
+            store_interface_suffix: "Store".to_string(),
+            helper_struct_suffix: "Helper".to_string(),
+            helper_interface_suffix: "Helper".to_string(),
+            store_variant_list: vec![
+                "Memory".to_string(),
+                "Mongo".to_string(),
+                "Postgresql".to_string(),
+                "Http".to_string(),
+            ],
+            params_struct_suffix: "Params".to_string(),
+            args_struct_suffix: "Args".to_string(),
+            result_struct_suffix: "Result".to_string(),
             framework: Framework::Echo,
         }
     }
