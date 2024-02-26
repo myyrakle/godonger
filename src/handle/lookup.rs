0ff7996d19a388d5f11700c0f9a8e8ed918ca3db
myyrakle <sssang97@naver.com>
2024-02-27 02:53:09 +0900

    [#1] config에 필드 추가

--- src/handle/lookup.rs
+++ src/handle/lookup.rs
@@ -31,6 +31,81 @@ pub fn lookup_domain(domain: String) -> Vec<PathBuf> {
     domain_file_path_list
 }
 
-pub fn lookup_internal(domain: String) {
+pub struct InternalFiles {
+    dir: PathBuf,
+    route: Option<RouteFiles>,
+    usecase: Option<UsecaseFiles>,
+    store: Option<StoreFiles>,
+    helper: Option<HelperFiles>,
+}
+
+impl Default for InternalFiles {
+    fn default() -> Self {
+        InternalFiles {
+            dir: PathBuf::new(),
+            route: None,
+            usecase: None,
+            store: None,
+            helper: None,
+        }
+    }
+}
+
+pub struct RouteFiles {
+    dir: PathBuf,
+    details: Vec<RouteFilesDetail>,
+}
+
+pub struct RouteFilesDetail {
+    dir: PathBuf,
+    filenames: Vec<String>,
+}
+
+pub struct StoreFiles {
+    dir: PathBuf,
+    details: Vec<StoreFilesDetail>,
+}
+
+pub struct StoreFilesDetail {
+    dir: PathBuf,
+    filenames: Vec<String>,
+}
+
+pub struct UsecaseFiles {
+    dir: PathBuf,
+    filenames: Vec<String>,
+}
+
+pub struct HelperFiles {
+    dir: PathBuf,
+    filenames: Vec<String>,
+}
+
+pub fn lookup_internal(domain: String) -> InternalFiles {
+    let mut internal_files = InternalFiles::default();
+
     let config_file = get_config_file_or_warn();
+
+    let internal_path = config_file.internal_dir;
+
+    let route_path = internal_path.join(&config_file.route_dir);
+    let usecase_path = internal_path.join(&config_file.usecase_dir);
+    let store_path = internal_path.join(&config_file.store_dir);
+    let helper_path = internal_path.join(&config_file.helper_dir);
+
+    if !internal_path.exists() {
+        panic!("Internal directory not found")
+    }
+    internal_files.dir = internal_path;
+
+    if route_path.exists() {
+        let route_files = RouteFiles {
+            dir: config_file.route_dir,
+            details: vec![],
+        };
+
+        internal_files.route = Some(route_files);
+    }
+
+    internal_files
 }
