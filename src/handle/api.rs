0281b785c4175424262cf4c7bb7deff876ff4047
myyrakle <sssang97@naver.com>
2024-02-28 00:05:51 +0900

    [#5] new_api handle 틀 구현

--- src/handle/api.rs
+++ src/handle/api.rs
@@ -1,3 +1,13 @@
 pub fn new_api(domain: String, api_path: String, method_name: String) {
-    println!("new_api");
+    // 1. create domain file if not exists
+
+    // 2. create handler file if not exists
+
+    // 3. create usecase file if not exists
+
+    // 4. add method to usecase interface of domain file
+
+    // 5. add method to usecase file
+
+    // 6. add method to handler file
 }
