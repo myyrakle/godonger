122f097723ffed7ad6605b743b49cd552eeaca1d
myyrakle <sssang97@naver.com>
2024-02-27 23:04:49 +0900

    [#5] api new action 템플릿 구현

---
+++ src/action/api/new.rs
@@ -0,0 +1,13 @@
+use crate::{
+    command::domain::list::ConfigOptions,
+    tui::{enter_tui, exit_tui},
+    utils::get_config_file_or_warn,
+};
+
+pub fn run(_: ConfigOptions) {
+    let config_file = get_config_file_or_warn();
+
+    let mut terminal = enter_tui();
+    //crate::tui::domain_list::run(&mut terminal, domain_list).unwrap();
+    exit_tui();
+}
