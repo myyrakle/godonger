4616b10c79726a6cd06f02d152133d0d5137867d
myyrakle <sssang97@naver.com>
2024-02-28 00:26:41 +0900

    [#5] add_usecase_interface_to_domain_file_if_not_exists 구현

--- src/handle/api.rs
+++ src/handle/api.rs
@@ -1,13 +1,21 @@
+use super::create_files::{
+    add_usecase_interface_to_domain_file_if_not_exists, create_domain_file_if_not_exists,
+};
+
 pub fn new_api(domain: String, api_path: String, method_name: String) {
     // 1. create domain file if not exists
+    create_domain_file_if_not_exists(domain.clone());
 
-    // 2. create handler file if not exists
+    // 2. add usecase interface to domain file if not exists
+    add_usecase_interface_to_domain_file_if_not_exists(domain.clone());
 
-    // 3. create usecase file if not exists
+    // 3. create handler file if not exists
 
-    // 4. add method to usecase interface of domain file
+    // 4. create usecase file if not exists
 
-    // 5. add method to usecase file
+    // 5. add method to usecase interface of domain file
 
-    // 6. add method to handler file
+    // 6. add method to usecase file
+
+    // 7. add method to handler file
 }
