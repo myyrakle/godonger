35589f017314aa7808361c0d838cd742e89a9daf
myyrakle <sssang97@naver.com>
2024-02-27 23:10:19 +0900

    [#5] 프로젝트 경로 정리

---
+++ src/command/api/mod.rs
@@ -0,0 +1,14 @@
+pub mod list;
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+pub struct Command {
+    #[clap(subcommand)]
+    pub action: SubCommand,
+}
+
+#[derive(clap::Subcommand, Debug)]
+pub enum SubCommand {
+    New(list::Command),
+}
