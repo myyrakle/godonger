a3add1bfbcedd400d02e4c06f83b92c50fdc25ba
myyrakle <sssang97@naver.com>
2024-03-04 01:39:53 +0900

    [#5] impl: generate_usecase_interface_method

--- src/handle/boilerplates.rs
+++ src/handle/boilerplates.rs
@@ -8,27 +8,37 @@ pub fn generate_usecase_interface_name(domain: String, config_file: &RootConfig)
     usecase_interface_name
 }
 
-pub fn generate_request_dto_name(method_name: String, config_file: &RootConfig) -> String {
+pub fn generate_request_dto_type(method_name: String, config_file: &RootConfig) -> String {
     let request_dto_name =
         method_name.as_str().to_case(Case::Pascal) + &config_file.request_dto_struct_suffix;
 
     request_dto_name
 }
 
-pub fn generate_response_dto_name(method_name: String, config_file: &RootConfig) -> String {
+pub fn generate_response_dto_type(method_name: String, config_file: &RootConfig) -> String {
     let response_dto_name =
         method_name.as_str().to_case(Case::Pascal) + &config_file.response_dto_struct_suffix;
 
     response_dto_name
 }
 
-pub fn generate_usecase_interface_method(method_name: String, has_response: bool) -> String {
+pub fn generate_usecase_interface_method(
+    method_name: String,
+    has_response: bool,
+    config_file: &RootConfig,
+) -> String {
     let method_name = method_name.as_str().to_case(Case::Pascal);
 
+    let request_dto_type = generate_request_dto_type(method_name.clone(), config_file);
+
     let method_signature = if has_response {
-        format!(r#"{}() (string, error)"#, method_name)
+        let response_dto_type = generate_response_dto_type(method_name.clone(), config_file);
+
+        format!(
+            r#"{method_name}(ctx context.Context, request {request_dto_type}) ({response_dto_type}, error)"#
+        )
     } else {
-        format!(r#"{}() error"#, method_name)
+        format!(r#"{method_name}(ctx context.Context) error"#)
     };
 
     method_signature
