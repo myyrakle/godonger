d44d5ce361be5a1eb2939a3175df800611211575
myyrakle <sssang97@naver.com>
2024-03-04 01:37:27 +0900

    [#5] add: generate_request_dto_name, generate_response_dto_name

--- src/handle/boilerplates.rs
+++ src/handle/boilerplates.rs
@@ -1,9 +1,35 @@
 use crate::config::RootConfig;
 use convert_case::{Case, Casing};
 
-pub fn generate_usecase_inteface_name(domain: String, config_file: &RootConfig) -> String {
+pub fn generate_usecase_interface_name(domain: String, config_file: &RootConfig) -> String {
     let usecase_interface_name =
         domain.as_str().to_case(Case::Pascal) + &config_file.usecase_interface_suffix;
 
     usecase_interface_name
+}
+
+pub fn generate_request_dto_name(method_name: String, config_file: &RootConfig) -> String {
+    let request_dto_name =
+        method_name.as_str().to_case(Case::Pascal) + &config_file.request_dto_struct_suffix;
+
+    request_dto_name
+}
+
+pub fn generate_response_dto_name(method_name: String, config_file: &RootConfig) -> String {
+    let response_dto_name =
+        method_name.as_str().to_case(Case::Pascal) + &config_file.response_dto_struct_suffix;
+
+    response_dto_name
+}
+
+pub fn generate_usecase_interface_method(method_name: String, has_response: bool) -> String {
+    let method_name = method_name.as_str().to_case(Case::Pascal);
+
+    let method_signature = if has_response {
+        format!(r#"{}() (string, error)"#, method_name)
+    } else {
+        format!(r#"{}() error"#, method_name)
+    };
+
+    method_signature
 }
--- src/handle/edit_files.rs
+++ src/handle/edit_files.rs
@@ -2,14 +2,14 @@ use std::fs;
 
 use crate::utils::get_config_file_or_warn;
 
-use super::boilerplates::generate_usecase_inteface_name;
+use super::boilerplates::generate_usecase_interface_name;
 
 pub fn add_usecase_interface_to_domain_file_if_not_exists(domain: String) {
     let config_file = get_config_file_or_warn();
 
     let domain_file_path = config_file.domain_dir.join(domain.clone() + ".go");
 
-    let usecase_interface_name = generate_usecase_inteface_name(domain.clone(), &config_file);
+    let usecase_interface_name = generate_usecase_interface_name(domain.clone(), &config_file);
 
     let mut domain_file_content = fs::read_to_string(&domain_file_path).unwrap();
 
@@ -24,4 +24,30 @@ pub fn add_usecase_interface_to_domain_file_if_not_exists(domain: String) {
     }
 }
 
-pub fn add_method_to_usecase_interface_of_domain_file(domain: String, method_name: String) {}
+pub fn add_method_to_usecase_interface_of_domain_file(domain: String, method_name: String) {
+    let config_file = get_config_file_or_warn();
+
+    let domain_file_path = config_file.domain_dir.join(domain.clone() + ".go");
+
+    let usecase_interface_name = generate_usecase_interface_name(domain.clone(), &config_file);
+
+    let mut domain_file_content = fs::read_to_string(&domain_file_path).unwrap();
+
+    if domain_file_content.contains(&usecase_interface_name) {
+        let usecase_interface_name = usecase_interface_name.as_str();
+
+        let method_name = method_name.as_str();
+
+        let method_signature = format!(r#"{}() (string, error)"#, method_name);
+
+        if !domain_file_content.contains(&method_signature) {
+            domain_file_content.push_str("\n\n");
+
+            domain_file_content.push_str(format!(r#"    // {}"#, method_name).as_str());
+            domain_file_content.push_str("\n");
+            domain_file_content.push_str(format!(r#"    {}"#, method_signature).as_str());
+
+            fs::write(domain_file_path, domain_file_content).unwrap();
+        }
+    }
+}
